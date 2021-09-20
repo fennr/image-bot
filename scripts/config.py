3132b85248ba082c8a9800454eb8312b62b3c32b
fennr <fenrir1121@gmail.com>
2021-09-20 22:13:48 +0500

    update config read

--- scripts/config.py
+++ scripts/config.py
@@ -8,6 +8,6 @@ def read_config():
     if not os.path.isfile(path):
         sys.exit("'config.yaml' not found! Please add it and try again.")
     else:
-        with open(path) as file:
+        with open(path, encoding='utf-8') as file:
             config = yaml.load(file, Loader=yaml.FullLoader)
     return config
